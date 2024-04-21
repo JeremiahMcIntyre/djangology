5a5774bcd2ee87544f4733e8a7b836745741e02f
Jeremaih McIntyre <jeremiahmcintyre@gmail.com>
2024-04-21 15:56:58 -0500

    now, I'm ready to merge

--- djangology/djangology/urls.py
+++ djangology/djangology/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from authentication import views
+from player import views
 from django.conf import settings
 from django.conf.urls.static import static
 
